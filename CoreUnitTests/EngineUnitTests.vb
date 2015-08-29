31e30d0146c1b7865ac4c382bd2b4b4e5df60699
Jon Steven <bluefootedpig@gmail.com>
2015-08-28 19:15:19 -0700

    Fixed some unit tests

--- CoreUnitTests/EngineUnitTests.vb
+++ CoreUnitTests/EngineUnitTests.vb
@@ -15,8 +15,43 @@ Public Class EngineUnitTests
 
     <TestMethod()>
     Public Sub Engine_ExecuteStrageties()
-        Assert.Inconclusive()
+        'setup
+        Dim mockMarket As IMarket = Substitute.For(Of IMarket)()
+        Dim mockCompany As Company = Substitute.For(Of Company)()
 
+        Dim testengine As New Engine(mockMarket)
+        testengine.Companies.Add(mockCompany)
+
+        'test
+        testengine.ExecuteComputerActions()
+
+        'verify
+        mockCompany.ReceivedWithAnyArgs().PerformAction(Nothing)
+
+    End Sub
+
+
+    <TestMethod()>
+    Public Sub Engine_ExecuteStrageties_ManyCompanies()
+        'setup
+        Dim mockMarket As IMarket = Substitute.For(Of IMarket)()
+        Dim mockCompany As Company = Substitute.For(Of Company)()
+
+        Dim testengine As New Engine(mockMarket)
+        testengine.Companies.Add(mockCompany)
+        Dim mockCompany2 = Substitute.For(Of Company)()
+        testengine.Companies.Add(mockCompany2)
+        Dim mockCompany3 = Substitute.For(Of Company)()
+        testengine.Companies.Add(mockCompany3)
+
+
+        'test
+        testengine.ExecuteComputerActions()
+
+        'verify
+        mockCompany.ReceivedWithAnyArgs().PerformAction(Nothing)
+        mockCompany2.ReceivedWithAnyArgs().PerformAction(Nothing)
+        mockCompany3.ReceivedWithAnyArgs().PerformAction(Nothing)
     End Sub
 
 
